23a8ee6bbb1f4eeaf4f56e93150b4bccad59fa76
kmalillos <malillosk@gmail.com>
2018-12-01 18:04:31 -0700

    added default searches

--- liri.js
+++ liri.js
@@ -27,6 +27,12 @@ var search = process.argv.slice(3).join("+");
 // =================================================================================================================================
 
 function concertThis() {
+
+    // default
+    if (!search) {
+        search = "the+rolling+stones"
+    }
+
     var queryUrl = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp";
     // console.log(queryUrl);
 
@@ -37,21 +43,20 @@ function concertThis() {
             var concert = response.data;
 
             //console.log(concert.length)
-
             // displays up to 5 songs
             if (concert.length > 5) {
                 for (var i = 0; i < 5; i++) {
                     console.log(`\n******************************************************\n`)
-                    console.log(`   Artist/Band: ${concert[i].lineup[0]}`) 
-                    console.log(`   Venue: ${concert[i].venue.name}`) 
+                    console.log(`   Artist/Band: ${concert[i].lineup[0]}`)
+                    console.log(`   Venue: ${concert[i].venue.name}`)
                     console.log(`   Location: ${concert[i].venue.city}, ${concert[i].venue.region}`)
                     console.log(`   Date: ${moment(concert[i].datetime).format("MM/DD/YYYY")}`)
                 };
                 console.log(`\n******************************************************`)
             } else {
                 for (var i = 0; i < concert.length; i++) {
-                    console.log(`   Artist/Band: ${concert[i].lineup[0]}`) 
-                    console.log(`   Venue: ${concert[i].venue.name}`) 
+                    console.log(`   Artist/Band: ${concert[i].lineup[0]}`)
+                    console.log(`   Venue: ${concert[i].venue.name}`)
                     console.log(`   Location: ${concert[i].venue.city}, ${concert[i].venue.region}`)
                     console.log(`   Date: ${moment(concert[i].datetime).format("MM/DD/YYYY")}`)
                 };
@@ -61,33 +66,45 @@ function concertThis() {
     );
 };
 
-// ***TO ADD "THE SIGN" DEFAULT***
 function spotifyThis() {
 
-        spotify.search({ type: 'track', query: search }, function (err, response) {
-            // console.log(response.tracks.items[0]);
+    //default 
+    if (!search) {
+        // search = "the+sign"
+        search = "never+gonna+give+you+up"
+    }
 
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
-            
-            // displays 5 songs
-            for (var i=0; i<5; i++) {
-                var track = response.tracks.items[i];
-    
-                console.log(`\n******************************************************\n`)
-                console.log(`   Artist/Band: ${track.artists[0].name}`) 
-                console.log(`   Song: ${track.name}`) 
-                console.log(`   Preview Song: ${track.external_urls.spotify}`)
-                console.log(`   Album: ${track.album.name}`)
-            };
-            console.log(`\n******************************************************`)
+    spotify.search({ type: 'track', query: search }, function (err, response) {
 
-        });
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        // console.log(response.tracks.items[0]);
+
+        // displays 5 songs
+        for (var i = 0; i < 5; i++) {
+            var track = response.tracks.items[i];
+
+            console.log(`\n******************************************************\n`)
+            console.log(`   Artist/Band: ${track.artists[0].name}`)
+            console.log(`   Song: ${track.name}`)
+            console.log(`   Preview Song: ${track.external_urls.spotify}`)
+            console.log(`   Album: ${track.album.name}`)
+        };
+        console.log(`\n******************************************************`)
+
+    });
 };
 
-// ***TO ADD "MR. NOBODY" DEFAULT***
 function movieThis() {
+
+    //default 
+    if (!search) {
+        // search = "mr+nobody"
+        search = "clueless"
+    }
+
     var queryUrl = "http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy";
     //console.log(queryUrl);
 
@@ -97,8 +114,8 @@ function movieThis() {
             var movie = response.data;
 
             console.log(`\n******************************************************\n`)
-            console.log(`   Title: ${movie.Title} `) 
-            console.log(`   Year: ${movie.Year} `) 
+            console.log(`   Title: ${movie.Title} `)
+            console.log(`   Year: ${movie.Year} `)
             console.log(`   IMBD Rating: ${movie.Ratings[0].Value}`)
             console.log(`   Rotten Tomatoes Rating: ${movie.Ratings[1].Value}`)
             console.log(`   Country: ${movie.Country}`)
@@ -112,7 +129,6 @@ function movieThis() {
         });
 };
 
-// do-what-it-says command
 function doThis() {
 
     fs.readFile("random.txt", "utf8", function (error, response) {
@@ -126,13 +142,15 @@ function doThis() {
         // Then split it by commas (to make it more readable)
         //.split breaks it into an array; splits at ", " (characters you want to split)
         var textArray = response.split(",");
-        
+
         // We will then re-display the content as an array for later use.
         // console.log(textArray);
 
-        for (var i=0; i<textArray.length; i++) {
+        for (var i = 0; i < textArray.length; i++) {
             // console.log(textArray[i])
 
+            // IF index ends in even number (0,2,4,6,8), store variable as the "command"
+            // ELSE index ends in odd number (1,3,5,7,9) store variable as the "search"
             if (i % 2 === 0) {
                 command = textArray[i];
             } else {
@@ -140,9 +158,9 @@ function doThis() {
             }
 
         };
-        
+
         console.log(command, search);
-        
+
         if (command == "concert-this") {
             concertThis();
         } else if (command == "spotify-this-song") {
@@ -155,7 +173,7 @@ function doThis() {
             console.log(`\n   Try these commands: `)
             console.log(`\n   concert-this\n   spotify-this-song\n   movie-this\n   do-what-it-says\n   say-hi`)
             console.log(`\n******************************************************`)
-        }    
+        }
 
     })
 };
@@ -166,7 +184,7 @@ function sayHi() {
     |_|  _  | |  _  |    | / ._ _    |   |  |_)  |  | 
     | | (/_ | | (_) o   _|_  | | |   |_ _|_ | | _|_ o
     
-    `);                                                                            
+    `);
 };
 
 // =================================================================================================================================
@@ -189,6 +207,5 @@ if (command == "concert-this") {
     console.log(`\n   Try these commands: `)
     console.log(`\n   concert-this\n   spotify-this-song\n   movie-this\n   do-what-it-says\n   say-hi`)
     console.log(`\n******************************************************`)
-}    
+}
 
-    
